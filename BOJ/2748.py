93b0f23a6abbbfce2225706e6aeb755ca213b56d
ggpp0909 <84262873+ggpp0909@users.noreply.github.com>
2021-07-20 01:10:01 +0900

    Create 2748.py

---
+++ BOJ/2748.py
@@ -0,0 +1,16 @@
+n=int(input())
+dp=[None for i in range(n+10)]
+
+def fibo(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    
+    if dp[n] != None:
+        return dp[n]
+
+    dp[n] = fibo(n-1) + fibo(n-2)
+    return dp[n]
+
+print(fibo(n)) 
