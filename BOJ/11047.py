96d104bde0bca3cac3003aa139bbac6ece9858c2
장영남 <ggpp0909@naver.com>
2021-08-06 00:59:14 +0900

    210806 || upload

--- BOJ/11047.py
+++ BOJ/11047.py
@@ -6,11 +6,8 @@ for i in range(n):
 
 cnt = 0
 for i in coin[::-1]:
-    if i > m:
-        continue
-
-    while m >= i :
-        m -= i
-        cnt += 1
+    if m >= i:
+        cnt += m // i
+        m %= i
     
 print(cnt)
---
+++ BOJ/1439.py
@@ -0,0 +1,10 @@
+a = input()
+cnt = 0
+for i in range(len(a)):
+    if a[i-1] != a[i]:
+        cnt += 1
+
+if cnt % 2 == 0:
+    print(cnt//2)
+else:
+    print(cnt//2 + 1)
