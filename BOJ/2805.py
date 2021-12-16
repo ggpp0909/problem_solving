1e5320f42fbc0093e4bd4755c898ed192e469a51
장영남 <ggpp0909@naver.com>
2021-12-16 21:24:54 +0900

    upload

--- BOJ/2805.py
+++ BOJ/2805.py
@@ -1,23 +1,20 @@
 import sys
-
-n, m = map(int, sys.stdin.readline().rstrip().split())
+N, M = map(int, sys.stdin.readline().rstrip().split())
 arr = list(map(int, sys.stdin.readline().rstrip().split()))
-
 
 s = 0
 e = 1000000000
+ans = 0
 while s <= e:
     mid = (s + e) // 2
-
     tot = 0
     for i in arr:
-        if i - mid > 0:
-            tot += i-mid
-    
-    if tot >= m: 
-        s = mid + 1 #톱 높이를 높여
-    
+        tot += max(0, i - mid)
+
+    if tot >= M:
+        ans = mid
+        s = mid + 1
     else:
-        e = mid - 1 #톱 높이를 줄여
-    
-print(e)
+        e = mid - 1
+
+print(ans)
