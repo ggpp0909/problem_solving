f5e00d61fe3fbdd4211a802ac338ae8713b04575
SSAFY <ggpp0909@naver.com>
2022-01-03 23:26:10 +0900

    upload

---
+++ BOJ/1000.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner apple = new Scanner(System.in);
+        int A = apple.nextInt();
+        int B = apple.nextInt();
+        apple.close();
+            
+        System.out.println(A + B);
+    }
+}
