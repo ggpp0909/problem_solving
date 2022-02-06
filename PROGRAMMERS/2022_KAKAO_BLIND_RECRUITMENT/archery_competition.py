3e5e015cb6c235660b521c1942d4d4b45b416395
장영남 <ggpp0909@naver.com>
2022-02-06 23:12:24 +0900

    upload

---
+++ PROGRAMMERS/2022_KAKAO_BLIND_RECRUITMENT/archery_competition.py
@@ -0,0 +1,36 @@
+def solution(n, info):
+    ans_score = 0
+    ans_cur = 0
+    ans = [-1]
+        
+    
+    def recur(cur, cnt, n, apeach, lion):
+        nonlocal ans_score, ans, ans_cur
+        
+        if cnt > n or cur == 11:
+            return
+        if cnt == n and cur == 10:
+            apeachscore = 0
+            lionscore = 0
+            for i in range(10):
+                if apeach[i] == lion[i] == 0:
+                    continue
+                elif apeach[i] >= lion[i]:
+                    apeachscore += 10 - i
+                else:
+                    lionscore += 10 - i
+            if lionscore > apeachscore and lionscore - apeachscore >= ans_score:
+                ans_score = lionscore - apeachscore
+                lion[10] = n - cnt
+                ans = lion[:]
+                ans_cur = cur
+                    
+        # 어피치 화살보다 한개더 많이쏘거나 아예 안쏘거나
+        recur(cur + 1, cnt, n, apeach, lion)
+        lion[cur] = apeach[cur] + 1
+        recur(cur + 1, cnt + apeach[cur] + 1, n, apeach, lion)
+        lion[cur] = 0
+    
+    recur(0, 0, n, info, [0] * 11)
+    
+    return ans
