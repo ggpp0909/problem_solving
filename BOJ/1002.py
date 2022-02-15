ccb158a53b518bd726e85df6b9f8665e6c82303f
장영남 <ggpp0909@naver.com>
2022-02-15 21:21:02 +0900

    upload

--- BOJ/1002.py
+++ BOJ/1002.py
@@ -2,7 +2,6 @@ n = int(input())
 for k in range(n):
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
     dis = abs(complex(x1 - x2, y1 - y2))
-    
     if dis == 0:
         if r1 == r2:
             print(-1)
