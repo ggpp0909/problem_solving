dcdc9b34f12b09fb71bccae7db3854bf0baeef38
장영남 <ggpp0909@naver.com>
2022-02-28 04:56:37 +0900

    upload

--- BOJ/11404.py
+++ BOJ/11404.py
@@ -1,40 +1,63 @@
-import heapq
-import sys
+# 다익스트라
 
-n = int(sys.stdin.readline().rstrip())
-m = int(sys.stdin.readline().rstrip())
-v = [[] for i in range(n + 1)]
+# import heapq
+# import sys
+#
+# n = int(sys.stdin.readline().rstrip())
+# m = int(sys.stdin.readline().rstrip())
+# v = [[] for i in range(n + 1)]
+# INF = int(1e9)
+#
+# for i in range(m):
+#     a, b, c = map(int, sys.stdin.readline().rstrip().split())
+#     v[a].append([b, c])
+#
+# def dijkstra(i):
+#     dist = [INF for k in range(n + 1)]
+#     que = []
+#     dist[i] = 0
+#     heapq.heappush(que, [0, i])
+#     while que:
+#         d, s = heapq.heappop(que)
+#
+#         if d > dist[s]:
+#             continue
+#
+#         for k in v[s]:
+#             nd = d + k[1]
+#
+#             if dist[k[0]] > nd:
+#                 dist[k[0]] = nd
+#                 heapq.heappush(que, [nd, k[0]])
+#
+#     for k in range(1, n + 1):
+#         if dist[k] == INF:
+#             print(0, end=' ')
+#         else:
+#             print(dist[k], end=' ')
+#
+# for i in range(1, n + 1):
+#     dijkstra(i)
+#     print()
+#
+
+# 플로이드 와샬
+n = int(input())
+m = int(input())
+INF = int(1e9)
+dp = [[INF for i in range(n)] for j in range(n)]
 
 for i in range(m):
-    a, b, c = map(int, sys.stdin.readline().rstrip().split())
-    v[a].append([b, c])
+    a, b, c = map(int, input().split())
+    dp[a - 1][b - 1] = min(dp[a - 1][b - 1], c)
 
-def dijkstra(i):
-    dist = [999999 for k in range(n + 1)]
-    que = []
-    dist[i] = 0
-    heapq.heappush(que, [0, i])
-    while que:
-        d, s = heapq.heappop(que)
+for k in range(n): # 경유지
+    for i in range(n): # 출발지
+        for j in range(n): # 도착지
+            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
 
-        if d > dist[s]:
-            continue
+for i in range(n):
+    dp[i][i] = 0
 
-        for k in v[s]:
-            nd = d + k[1]
-
-            if dist[k[0]] > nd:
-                dist[k[0]] = nd
-                heapq.heappush(que, [nd, k[0]])
-
-    for k in range(1, n + 1):
-        if dist[k] == 999999:
-            print(0, end=' ')
-        else:
-            print(dist[k], end=' ')
-
-for i in range(1, n + 1):
-    dijkstra(i)
-    print()
-
-
+for i in dp:
+    print(*i)
