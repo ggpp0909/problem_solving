db5385335c2510a48225ee9abd8e2fd93b26547b
장영남 <ggpp0909@naver.com>
2022-05-29 22:56:33 +0900

    upload

--- BOJ/21312.py
+++ BOJ/21312.py
@@ -1,12 +1,12 @@
-a, b, c = map(int,input().split())
-if a%2==0 and b%2==0 and c%2==0:
-    ans=a*b*c
+a, b, c = map(int, input().split())
+if a % 2 == 0 and b % 2 == 0 and c % 2 == 0:
+    ans = a * b * c
 else:
-    if a%2==0:
-        a=1
-    if b%2==0:
-        b=1
-    if c%2==0:
-        c=1
-    ans=a*b*c
+    if a % 2 == 0:
+        a = 1
+    if b % 2 == 0:
+        b = 1
+    if c % 2 == 0:
+        c = 1
+    ans = a*b*c
 print(ans)
