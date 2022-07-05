a750d91a4d1fdc406604c237f33e292fada4d9f4
장영남 <ggpp0909@naver.com>
2022-07-05 23:53:33 +0900

    upload

--- BOJ/4435.py
+++ BOJ/4435.py
@@ -1,20 +1,20 @@
-T= int(input())
-gan_score=[1,2,3,3,4,10]
-sa_score=[1,2,2,2,3,5,10]
+T = int(input())
+gan_score = [1, 2, 3, 3, 4, 10]
+sa_score = [1, 2, 2, 2, 3, 5, 10]
 
 for i in range(T):
-    sa_sum=0
+    sa_sum = 0
     gan_sum = 0
-    gan= input().split()
-    sa= input().split()
+    gan = input().split()
+    sa = input().split()
     for k in range(len(gan)):
-        gan[k]= int(gan[k])
-        score= gan[k]*gan_score[k]
-        gan_sum=gan_sum+score
+        gan[k] = int(gan[k])
+        score = gan[k]*gan_score[k]
+        gan_sum = gan_sum+score
     for k in range(len(sa)):
-        sa[k]= int(sa[k])
-        score=sa[k]*sa_score[k]
-        sa_sum=sa_sum+score
+        sa[k] = int(sa[k])
+        score = sa[k]*sa_score[k]
+        sa_sum = sa_sum+score
 
     if gan_sum < sa_sum:
         print("Battle {0}: Evil eradicates all trace of Good".format(i+1))
