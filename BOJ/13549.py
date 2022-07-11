064937dd43a8575128ead1b1d81a5aebb92a2e06
장영남 <ggpp0909@naver.com>
2022-07-11 23:20:49 +0900

    upload

--- BOJ/13549.py
+++ BOJ/13549.py
@@ -1,7 +1,34 @@
+# from collections import deque
+
+# N, K = map(int, input().split())
+# visited = [999999 for i in range(100001)]
+
+# que = deque()
+# que.append([N, 0])  # [위치, 걸린시간]
+# visited[N] = True
+# ans = 100000
+
+# while que:
+#     location, time = que.popleft()
+
+#     if location == K:
+#         ans = min(ans, time)
+
+#     temp1 = location * 2
+#     temp2 = location + 1
+#     temp3 = location - 1
+
+#     for i in [[temp1, time], [temp2, time + 1], [temp3, time + 1]]:
+#         if 0 <= i[0] <= 100000 and i[1] <= visited[i[0]]:  # 조건, 가지치기
+#             visited[i[0]] = min(visited[i[0]], i[1])
+#             que.append([i[0], i[1]])
+
+# print(ans)
+
 from collections import deque
 
 N, K = map(int, input().split())
-visited = [False for i in range(100001)]
+visited = [999999 for i in range(100001)]
 
 que = deque()
 que.append([N, 0])  # [위치, 걸린시간]
@@ -19,8 +46,8 @@ while que:
     temp3 = location - 1
 
     for i in [[temp1, time], [temp2, time + 1], [temp3, time + 1]]:
-        if 0 <= i[0] <= 100000 and not visited[i[0]]:
-            visited[i[0]] = True
+        if 0 <= i[0] <= 100000 and visited[i[0]] > i[1]:  # 조건, 가지치기
+            visited[i[0]] = i[1]
             que.append([i[0], i[1]])
 
 print(ans)
