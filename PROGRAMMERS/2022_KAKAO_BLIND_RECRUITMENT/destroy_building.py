4819afc73b065b607b1a0800f53bfe6201bfd06f
장영남 <ggpp0909@naver.com>
2022-08-13 17:12:53 +0900

    upload

--- PROGRAMMERS/2022_KAKAO_BLIND_RECRUITMENT/destroy_building.py
+++ PROGRAMMERS/2022_KAKAO_BLIND_RECRUITMENT/destroy_building.py
@@ -1,13 +1,33 @@
 def solution(board, skill):
     N = len(board) # 행
     M = len(board[0]) # 열
-    prefix = [[0 for i in range(M + 1)] for j in range(N + 1)]
-    for i in range(1, N + 1):
-        for j in range(1, M + 1):
-            prefix[i][j] = prefix[i][j - 1] + prefix[i - 1][j] - prefix[i - 1][j - 1] + board[i - 1][j - 1]
-    print(prefix)
-    '''
-    doing
-    '''
+    imos = [[0 for i in range(M + 1)] for j in range(N + 1)]
+    
+    # 물감 짜기
+    for i in range(len(skill)):
+        type, r1, c1, r2, c2, degree = skill[i]
+        if type == 2: sign = 1 
+        else: sign = -1
+        
+        imos[r1][c1] += sign * degree
+        imos[r1][c2 + 1] -= sign * degree
+        imos[r2 + 1][c1] -= sign * degree
+        imos[r2 + 1][c2 + 1] += sign * degree     
+    
+    # 물감 오른쪽으로 칠하기
+    for i in range(N):
+        for j in range(1, M):
+            imos[i][j] += imos[i][j - 1]
+    # 물감 밑으로 칠하기
+    for j in range(M):
+        for i in range(1, N):
+            imos[i][j] += imos[i - 1][j]
+    
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] + imos[i][j] > 0:
+                cnt += 1
+
     answer = 0
-    return answer
+    return cnt
