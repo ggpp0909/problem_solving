5aa7ead3e198670653e33ea3b1245625f016f2d4
장영남 <ggpp0909@naver.com>
2022-08-15 23:45:02 +0900

    another method - prefix sum

--- PROGRAMMERS/2022_KAKAO_BLIND_RECRUITMENT/destroy_building.py
+++ PROGRAMMERS/2022_KAKAO_BLIND_RECRUITMENT/destroy_building.py
@@ -14,19 +14,27 @@ def solution(board, skill):
         imos[r2 + 1][c1] -= sign * degree
         imos[r2 + 1][c2 + 1] += sign * degree     
     
-    # 물감 오른쪽으로 칠하기
-    for i in range(N):
-        for j in range(1, M):
-            imos[i][j] += imos[i][j - 1]
-    # 물감 밑으로 칠하기
-    for j in range(M):
-        for i in range(1, N):
-            imos[i][j] += imos[i - 1][j]
+    # # 물감 오른쪽으로 칠하기
+    # for i in range(N):
+    #     for j in range(1, M):
+    #         imos[i][j] += imos[i][j - 1]
+    # # 물감 밑으로 칠하기
+    # for j in range(M):
+    #     for i in range(1, N):
+    #         imos[i][j] += imos[i - 1][j]
     
+    # cnt = 0
+    # for i in range(N):
+    #     for j in range(M):
+    #         if board[i][j] + imos[i][j] > 0:
+    #             cnt += 1
+    
+    prefix = [[0 for i in range(M + 1)] for j in range(N + 1)]
     cnt = 0
     for i in range(N):
         for j in range(M):
-            if board[i][j] + imos[i][j] > 0:
+            prefix[i + 1][j + 1] = prefix[i][j + 1] + prefix[i + 1][j] - prefix[i][j] + imos[i][j]
+            if board[i][j] + prefix[i + 1][j + 1] > 0:
                 cnt += 1
 
     answer = 0
