d754c37d90beb90328da3abe485dfc2d7a19c563
장영남 <ggpp0909@naver.com>
2022-08-27 01:38:17 +0900

    upload

---
+++ BOJ/25498.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+# 읿력 50만
+# 1. 단어길이가 가장 긴 단어들중에서 골라야한다.
+# 2. 그런 단어들중 사전순으로 골라야한다.
+# dfs + 가지치기인데 1 조건 만족못하면 return (들어갈 가지에 유망이없다)
+# 들어가면서 ord함수로 비교하면서 return
+
+
+N = int(input())
+word = input()
+v = [[] for i in range(N + 1)]
+edge_count = [0 for i in range(N + 1)] # 각 노드에
+
+for i in range(N - 1):
+    a, b = map(int, input().split())
+    v[a].append(b)
+    v[b].append(a)
+
+
+# 1. dfs한번 돌려서 각 노드의 최대깊이 저장하면서 return
+def find_edge_count(cur, cnt):
+    for i in v[cur]:
+        # 대충 짬 나중에 다시보기
+        edge_count[i] = find_edge_count(i, cnt + 1)
+        return cnt
+
