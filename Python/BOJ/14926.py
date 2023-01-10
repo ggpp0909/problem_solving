7b283dc46f29cde97fffa2bf7253f09d0e182f56
장영남 <ggpp09@gmail.com>
2023-01-10 21:31:11 +0900

    refactor

--- Python/BOJ/14926.py
+++ Python/BOJ/14926.py
@@ -4,18 +4,16 @@ sys.setrecursionlimit(10 ** 6)
 # 예제 출력을 하나씩 선으로 그어보면 완전 그래프가 나오는 것을 확인 가능
 # 모든 노드가 연결되어 있다고 인접 리스트 하고 dfs로 탐색
 N = int(input())
+v = [[] for i in range(N)] # 2차원
 
-v = [[] for i in range(N)]
-
+# N이 5일때, 인접리스트를 모두 [0, 1, 2, 3, ... N - 1] 로 세팅
 for i in range(N):
     v[i] = list(range(N))
 
 # print(v)
-
 visited = [[False for i in range(N)] for j in range(N)]
 visited[0][N - 1] = True
 visited[N - 1][0] = True # 첫 정점과 끝 정점은 따로 처리 (반례)
-
 
 def dfs(cur):
     print("a{}".format(cur + 1), end=" ")
@@ -28,7 +26,6 @@ def dfs(cur):
             visited[cur][i] = True
             visited[i][cur] = True
             dfs(i)
-
 
 dfs(0)
 print("a1")
